65e99e1e0a901669f10044ea3d9dc47ae49a8067
andreyko21 <andriy.zakharkiv.2001@gmail.com>
2023-07-03 21:11:41 +0300

    fix main-page js

---
+++ js/about-manufactures-swiper.js
@@ -0,0 +1,2 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){new Swiper(".about-manufactures__swiper",{slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})});
+//# sourceMappingURL=about-manufactures-swiper.js.map
---
+++ js/news-swiper.js
@@ -0,0 +1,2 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){new Swiper(".swiper-news-block-swiper",{slidesPerView:3,spaceBetween:30,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{0:{slidesPerView:1},601:{slidesPerView:2,spaceBetween:20},961:{slidesPerView:3,spaceBetween:30}}})});
+//# sourceMappingURL=news-swiper.js.map
