ce195ac9188eb2c6fc8e9a214e87f4838e02979f
andreyko21 <andriy.zakharkiv.2001@gmail.com>
2023-07-03 21:11:41 +0300

    fix main-page js

---
+++ dist/js/product-item-swiper.js
@@ -0,0 +1,2 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){new Swiper(".product-item__swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})});
+//# sourceMappingURL=product-item-swiper.js.map
