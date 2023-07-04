81fe0a18f19056fc81b2307821ed991d7e485a7b
andreyko21 <andriy.zakharkiv.2001@gmail.com>
2023-07-04 16:52:10 +0300

    fix css js contacts and news page

---
+++ dist/js/news-swiper-big.js
@@ -0,0 +1,2 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){new Swiper(".swiper-news-block-swiper-big",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})});
+//# sourceMappingURL=news-swiper-big.js.map
