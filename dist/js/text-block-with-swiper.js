d4dd30c13e65eecc9008239fc99370a7ab3fd761
andreyko21 <andriy.zakharkiv.2001@gmail.com>
2023-07-04 15:15:06 +0300

    fix css js about-company-page

---
+++ dist/js/text-block-with-swiper.js
@@ -0,0 +1,2 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){new Swiper(".text-block-with-swiper__swiper-block",{allowTouchMove:!1,slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})});
+//# sourceMappingURL=text-block-with-swiper.js.map
