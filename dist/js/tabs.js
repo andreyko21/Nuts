2a7f4a101753f5b9c6612a804f0fea693ccec8c6
andreyko21 <andriy.zakharkiv.2001@gmail.com>
2023-07-04 16:15:56 +0300

    fix css js wholesale-and-corporate-clients-page

---
+++ dist/js/tabs.js
@@ -0,0 +1,2 @@
+"use strict";function openTab(e,t){var s=document.getElementsByClassName("tabs-block__text-block-with-img");for(l=0;l<s.length;l++)s[l].style.display="none";document.getElementById(e).style.display="flex";for(var a=document.getElementsByClassName("tab-button"),l=0;l<a.length;l++)a[l].classList.remove("active");t.classList.add("active")}
+//# sourceMappingURL=tabs.js.map
