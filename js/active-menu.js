60cc25f61fa283d65597c14c52b1065bdb94fbd0
andreyko21 <andriy.zakharkiv.2001@gmail.com>
2023-07-04 15:15:06 +0300

    fix css js about-company-page

---
+++ js/active-menu.js
@@ -0,0 +1,2 @@
+"use strict";var currentUrl=window.location.href;document.addEventListener("DOMContentLoaded",function(){for(var e=window.location.href,t=document.getElementsByClassName("menu__item"),n=0;n<t.length;n++){var a=t[n];e===a.href&&a.classList.add("active")}});
+//# sourceMappingURL=active-menu.js.map
