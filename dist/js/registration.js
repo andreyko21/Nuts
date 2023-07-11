08aed9d7bae52cb713996cb6e867efb900141376
andreyko21 <andriy.zakharkiv.2001@gmail.com>
2023-07-11 13:34:19 +0300

    add registration page

---
+++ dist/js/registration.js
@@ -0,0 +1,2 @@
+"use strict";function previewImage(e){var t=new FileReader;t.onload=function(){document.getElementById("uploadImage").src=t.result},t.readAsDataURL(e.target.files[0])}
+//# sourceMappingURL=registration.js.map
